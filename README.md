041ffd034a68ca26025e72554494ebb9c0ba4ed1
nir0s <nir36g@gmail.com>
2014-08-04 10:09:58 +0300

    added extended apache dashboard

--- README.md
+++ README.md
@@ -8,19 +8,22 @@ A logging workshop backed by the ELK Trio.
 * [Virtualbox](https://www.virtualbox.org/)
 * [Vagrant](http://www.vagrantup.com/)
 
-# Provisioner
+# Vagrant
+
+## Provisioner
 
 The vagrant directory contains a Vagrantfile that provisions a machine with:
 
 * [Elasticsearch](http://www.elasticsearch.org/overview/elasticsearch)
 * [Elasticsearch-head plugin](http://mobz.github.io/elasticsearch-head/)
+* [Elasticsearch-kopf plugin](https://github.com/lmenezes/elasticsearch-kopf)
 * [Logstash](http://www.elasticsearch.org/overview/logstash)
 * [Kibana](http://www.elasticsearch.org/overview/kibana)
 * [feeder](http://feeder.readthedocs.org)
 * [Nginx](http://nginx.com/)
 * [RabbitMQ](http://www.rabbitmq.com/)
 
-# Box
+## Box
 
 The Vagrantfile in the root directory will load a box created from the above provisioned machine.
 
@@ -36,4 +39,18 @@ The resources directory contains:
 
 [feeder](http://github.com/nir0s/feeder) generates random logs which you can use during the workshop.
 
+For instance, from the ~/elk-workshop directory, run:
+
+```bash
+mouth feed -f ApacheAccess -t File -m 1000 -g 0.001 -b 200
+```
+
+This will write an ApacheAccessEx log, to a file in the current directory called generated.log.
+
 Please refer to the [documentation](http://feeder.readthedocs.org) to understand how to use it.
+
+# Running logstash
+
+```bash
+~/elk/logstash/bin/logstash -f ~/elk-workshop/resources/logstash.conf
+```
--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -4,7 +4,7 @@ echo bootstrapping...
 sudo apt-get -y update &&
 
 # install prereqs
-sudo apt-get install -y vim openjdk-7-jdk python-dev curl
+sudo apt-get install -y vim openjdk-7-jdk python-dev curl git
 
 # install pip
 curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python
@@ -38,8 +38,9 @@ sudo update-rc.d elasticsearch defaults 95 10
 sudo /etc/init.d/elasticsearch start
 rm elasticsearch.deb
 
-# install elasticsearch-head plugin
-sudo /usr/share/elasticsearch/bin/plugin -install mobz/elasticsearch-head
+# install elasticsearch plugins
+sudo /usr/share/elasticsearch/bin/plugin --install mobz/elasticsearch-head
+sudo /usr/share/elasticsearch/bin/plugin --install lmenezes/elasticsearch-kopf/1.2
 
 # install kibana
 mkdir kibana
@@ -48,7 +49,7 @@ tar -xzvf kibana.tar.gz -C kibana --strip 1
 sed -i 's/"+window.location.hostname+"/11.0.1.7/g' kibana/config.js
 rm kibana.tar.gz
 
-# install kibana
+# install nginx
 sudo apt-get install -y nginx
 sudo cp /home/vagrant/resources/nginx.conf /etc/nginx/conf.d/
 sudo service nginx start
@@ -58,8 +59,12 @@ sudo apt-get install -y erlang-nox
 wget http://www.rabbitmq.com/releases/rabbitmq-server/v3.2.4/rabbitmq-server_3.2.4-1_all.deb -O rabbitmq.deb
 sudo dpkg -i rabbitmq.deb
 
+# clone workshop repo
+
+git clone http://github.com/nir0s/elk-workshop
+
 # set shell login base dir
-echo "cd ~/elk" >> /home/vagrant/.bashrc
+echo "cd ~/elk-workshop" >> /home/vagrant/.bashrc
 
 # source virtualenv on login
 echo "source /home/vagrant/feeder/bin/activate" >> /home/vagrant/.bashrc
